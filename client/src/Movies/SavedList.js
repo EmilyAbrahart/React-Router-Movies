c2ed77554e5d71b97917293ff92550c775b75fae
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-10 13:08:13 +0100

    Add functionality to save a movie

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
-        <Link to='/'><div className="home-button">Home</div></Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="saved-list">
+				<h3>Saved Movies:</h3>
+				{this.props.list.map(movie => (
+					<span className="saved-movie">{movie.title}</span>
+				))}
+				<Link to="/">
+					<div className="home-button">Home</div>
+				</Link>
+			</div>
+		);
+	}
 }
