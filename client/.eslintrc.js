de80d4f3203b5e0bcc9c511a884179ca21b97892
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-10 13:40:40 +0100

    fix eslint no-unused-vars rule linting for react and issue with saved movie link

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -19,5 +20,8 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/jsx-uses-vars": 2,
+        "react/prop-types": 0,
+        "no-console": 0,
     }
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -10,7 +10,11 @@ export default class SavedList extends Component {
 			<div className="saved-list">
 				<h3>Saved Movies:</h3>
 				{this.props.list.map(movie => (
-					<Link to={`movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></Link>
+					<Link exact to={`/movies/${movie.id}`} key={movie.id}>
+						<span className="saved-movie" >
+							{movie.title}
+						</span>
+					</Link>
 				))}
 				<Link to="/">
 					<div className="home-button">Home</div>
