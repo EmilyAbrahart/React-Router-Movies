298d96c17de5384a9e5ccb705f4035c2e4c5b30e
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-10 13:12:04 +0100

    Saved Movie links and refactor imports

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,40 +1,42 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: []
+		};
+	}
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/api/movies')
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
-  }
+	componentDidMount() {
+		axios
+			.get('http://localhost:5000/api/movies')
+			.then(response => {
+				this.setState(() => ({ movies: response.data }));
+			})
+			.catch(error => {
+				console.error('Server Error', error);
+			});
+	}
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="movie-list">
+				{this.state.movies.map(movie => (
+					<MovieDetails key={movie.id} movie={movie} />
+				))}
+			</div>
+		);
+	}
 }
 
 function MovieDetails({ movie }) {
-  const {id} = movie;
-  return (
-  <Link to={`movies/${id}`}><MovieCard {...movie} /></Link>
-  );
+	const { id } = movie;
+	return (
+		<Link to={`movies/${id}`}>
+			<MovieCard {...movie} />
+		</Link>
+	);
 }
